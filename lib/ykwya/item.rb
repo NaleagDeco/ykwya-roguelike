480f25d8cf0206ae154a25fdbdac9003e187492a
Gaelan D'costa <gdcosta@gmail.com>
2013-09-20 14:47:27 -0400

    Added a PotionFactory class

--- lib/ykwya/item.rb
+++ lib/ykwya/item.rb
@@ -1,8 +1,33 @@
 require 'ykwya/game-piece'
 
+##
+# A helper method to convert StudlyCaps to underscore-delimited format
+class String
+  # ruby mutation methods have the expectation to return self if a mutation occurred, nil otherwise. (see http://www.ruby-doc.org/core-1.9.3/String.html#method-i-gsub-21)
+  def to_underscore!
+    g = gsub!(/(.)([A-Z])/,'\1_\2'); d = downcase!
+    g || d
+  end
+
+  def to_underscore
+    dup.tap { |s| s.to_underscore! }
+  end
+end
+
 module YKWYA
   class Potion
     include GamePieceMixin
+
+    def self.potion_seed
+      [
+       ['RestoreHealth', 10, :@hitpoints],
+       ['PoisonHealth', -10, :@hitpoints],
+       ['BoostAttack', 5, :@attack],
+       ['WoundAttack', -5, :@attack],
+       ['BoostDefense', 5, :@defense],
+       ['WoundDefense', -5, :@defense]
+      ]
+    end
 
     ##
     # What attribute is this potion supposed to adjust?
@@ -26,6 +51,16 @@ module YKWYA
 
   class Gold
     include GamePieceMixin
+  end
+
+  class PotionFactory
+    class << self
+      Potion.potion_seed.each do |potion|
+        self.send(:define_method, potion[0].to_underscore) do
+          Potion.new(potion[1], potion[2])
+        end
+      end
+    end
   end
 
 end
---
+++ spec/item_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'PotionFactory' do
+  def self.has_potion name, magnitude, attribute
+    it 'creates #{magnitude} #{attribute} potion with #{name} function.' do
+      potion = PotionFactory.send(name.to_underscore)
+      potion.magnitude.should be magnitude
+      potion.attribute.should be attribute
+    end
+  end
+
+  has_potion 'RestoreHealth', 10, :@hitpoints
+  has_potion 'PoisonHealth', -10, :@hitpoints
+
+  has_potion 'BoostAttack', 5, :@attack
+  has_potion 'WoundAttack', -5, :@attack
+
+  has_potion 'BoostDefense', 5, :@defense
+  has_potion 'WoundDefense', -5, :@defense
+end
